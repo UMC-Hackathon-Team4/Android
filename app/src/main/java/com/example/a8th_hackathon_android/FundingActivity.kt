380bfd28acf0ad36a2a7eccaf426e6da292ca532
james4510 <james451055@gmail.com>
2025-07-05 23:41:57 +0900

    fix:background

--- app/src/main/java/com/example/a8th_hackathon_android/FundingActivity.kt
+++ app/src/main/java/com/example/a8th_hackathon_android/FundingActivity.kt
@@ -16,32 +16,7 @@ class FundingActivity : AppCompatActivity() {
         binding = ActivityFundingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 작품 정보 확장/축소 토글 처리
-        binding.btnToggleInfo.setOnClickListener {
-            val isVisible = binding.workInfoDetail.visibility == View.VISIBLE
-            binding.workInfoDetail.visibility = if (isVisible) View.GONE else View.VISIBLE
-            binding.btnToggleInfo.setImageResource(
-                if (isVisible) R.drawable.ic_arrow_down else R.drawable.ic_arrow_up
-            )
-        }
 
-        // 스토리 작성 확장/축소 토글 처리
-        binding.btnToggleStory.setOnClickListener{
-            val isVisible = binding.storyDetail.visibility == View.VISIBLE
-            binding.storyDetail.visibility = if (isVisible) View.GONE else View.VISIBLE
-            binding.btnToggleStory.setImageResource(
-                if (isVisible) R.drawable.ic_arrow_down else R.drawable.ic_arrow_up
-            )
-        }
-
-        // 리워드 작성 확장/축소 토클 처리
-        binding.btnToggleReward.setOnClickListener{
-            val isVisible = binding.rewardDetail.visibility == View.VISIBLE
-            binding.rewardDetail.visibility = if (isVisible) View.GONE else View.VISIBLE
-            binding.btnToggleReward.setImageResource(
-                if (isVisible) R.drawable.ic_arrow_down else R.drawable.ic_arrow_up
-            )
-        }
     }
 
 }
